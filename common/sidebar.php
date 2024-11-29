be55b4e8d4a9ff46d73526031ed4c56edb04c170
Safalya Kumbhare <safalyakumbhare@gmail.com>
2024-11-29 09:47:21 +0530

    Side bar updated

--- common/sidebar.php
+++ common/sidebar.php
@@ -96,41 +96,8 @@
               <i class="menu-arrow"></i>
             </a>
           </li>
-          <li class="nav-item menu-items">
-            <a class="nav-link" href="pages/icons/font-awesome.html">
-              <span class="menu-icon">
-                <i class="mdi mdi-contacts"></i>
-              </span>
-              <span class="menu-title">Icons</span>
-              <i class="menu-arrow"></i>
-            </a>
-          </li>
-          <li class="nav-item menu-items">
-            <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-              <span class="menu-icon">
-                <i class="mdi mdi-security"></i>
-              </span>
-              <span class="menu-title">User Pages</span>
-              <i class="menu-arrow"></i>
-            </a>
-            <div class="collapse" id="auth">
-              <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
-                <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
-                <li class="nav-item"> <a class="nav-link" href="pages/samples/error-404.html"> 404 </a></li>
-                <li class="nav-item"> <a class="nav-link" href="pages/samples/error-500.html"> 505 </a></li>
-                <li class="nav-item"> <a class="nav-link" href="pages/samples/blank-page.html"> Blank Page </a></li>
-              </ul>
-            </div>
-          </li>
-          <li class="nav-item menu-items">
-            <a class="nav-link" href="docs/documentation.html">
-              <span class="menu-icon">
-                <i class="mdi mdi-file-document"></i>
-              </span>
-              <span class="menu-title">Documentation</span>
-            </a>
-          </li>
+         
+        
         </ul>
       </nav>
       <!-- partial -->
